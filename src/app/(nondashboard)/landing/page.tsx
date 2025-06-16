0553e44d5f19512576ad864f99daebd41f62a1b2
robim16 <carteagajimenez@gmail.com>
2025-06-16 15:44:31 -0500

    creacion del footer section

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import HeroSection from './HeroSection'
-import FeatureSection from './FeatureSection'
+import FeatureSection from './FeaturesSection'
+import DiscoverSection from './DiscoverSection'
+import CallToActionSection from './CallToActionSection'
+import FooterSection from './FooterSection'
 
 const Landing = () => {
   return (
     <div>
       <HeroSection />
       <FeatureSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
     </div>
   )
 }
