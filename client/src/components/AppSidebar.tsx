77b554c84db8afeb9e247943a98d0bf556dd163b
robim16 <carteagajimenez@gmail.com>
2025-07-13 15:44:29 -0500

    creacion del tenants section

--- client/src/components/AppSidebar.tsx
+++ client/src/components/AppSidebar.tsx
@@ -1,10 +1,79 @@
 import { usePathname } from 'next/navigation'
 import React from 'react'
+import { Sidebar, SidebarHeader, SidebarMenu, SidebarMenuItem, useSidebar } from './ui/sidebar'
+import { Building, FileText, Heart, Menu, Settings, X } from 'lucide-react'
+import Home from '@/app/page'
+import { NAVBAR_HEIGHT } from '@/lib/constants'
+import { cn } from '@/lib/utils'
 
 const AppSidebar = ({ userType }: AppSidebarProps) => {
-    const pathname = usePathname()
+  const pathname = usePathname()
+  const { toggleSidebar, open } = useSidebar()
+
+  const navLinks =
+    userType === "manager"
+      ? [
+        { icon: Building, label: "Properties", href: "/managers/properties" },
+        {
+          icon: FileText,
+          label: "Applications",
+          href: "/managers/applications",
+        },
+        { icon: Settings, label: "Settings", href: "/managers/settings" },
+      ]
+      : [
+        { icon: Heart, label: "Favorites", href: "/tenants/favorites" },
+        {
+          icon: FileText,
+          label: "Applications",
+          href: "/tenants/applications",
+        },
+        { icon: Home, label: "Residences", href: "/tenants/residences" },
+        { icon: Settings, label: "Settings", href: "/tenants/settings" },
+      ];
   return (
-    <div>AppSidebar</div>
+    <Sidebar
+      collapsible='icon'
+      className='fixed left-0 bg-white shadow-lg'
+      style={{
+        top: `${NAVBAR_HEIGHT}`,
+        height: `calc(100vh - ${NAVBAR_HEIGHT}px)`
+      }}
+    >
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <div
+              className={cn(
+                "flex min-h-[56px] w-full items-center pt-3 mb-3",
+                open ? "justify-between px-6" : "justify-center"
+              )}
+            >
+              {open ? (
+                <>
+                  <h1 className="text-xl font-bold text-gray-800">
+                    {userType === "manager" ? "Manager View" : "Renter View"}
+                  </h1>
+                  <button
+                    className="hover:bg-gray-100 p-2 rounded-md"
+                    onClick={() => toggleSidebar()}
+                  >
+                    <X className="h-6 w-6 text-gray-600" />
+                  </button>
+                </>
+              ) : (
+                <button
+                  className="hover:bg-gray-100 p-2 rounded-md"
+                  onClick={() => toggleSidebar()}
+                >
+                  <Menu className="h-6 w-6 text-gray-600" />
+                </button>
+              )}
+            </div>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+    </Sidebar>
   )
 }
 
