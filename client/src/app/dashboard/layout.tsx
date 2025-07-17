f612a7ed482e56d66d72a7dc26668ec8bd75af49
robim16 <carteagajimenez@gmail.com>
2025-07-17 15:44:44 -0500

    cambios en tenants favorites

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -1,24 +1,54 @@
-import Navbar from '@/components/Navbar'
-import { Sidebar, SidebarProvider } from '@/components/ui/sidebar'
-import { NAVBAR_HEIGHT } from '@/lib/constants'
-import React from 'react'
+"use client"
 
-const DashboardLayout = ({ children }: {children: React.ReactNode}) => {
-  return (
-    <SidebarProvider>
-        <div className='min-h-screen w-full bg-primary-100'>
-            <Navbar />
-            <div style={{ paddingTop: `${NAVBAR_HEIGHT}px`}}>
-                <main className='flex'>
-                    <Sidebar />
-                    <div className="flex-grouw transition-all duration-300">
-                        {children}
-                    </div>
-                </main>
+import Navbar from '@/components/Navbar'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { NAVBAR_HEIGHT } from '@/lib/constants'
+import Sidebar from "@/components/AppSidebar"
+import React, { useEffect, useState } from 'react'
+import { useGetAuthUserQuery } from '@/state/api'
+import { usePathname, useRouter } from 'next/navigation'
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    const { data: authUser } = useGetAuthUserQuery()
+    const router = useRouter()
+    const pathname = usePathname()
+    const [isLoading, setIsLoading] = useState(true)
+
+
+    useEffect(() => {
+        if (authUser) {
+            const userRole = authUser.userRole?.toLowerCase()
+            if (
+                (userRole === "manager" && pathname.startsWith("/tenant")) ||
+                (userRole === "tenant" && pathname.startsWith("/managers"))
+            ) {
+                router.push(
+                    userRole === "manager"
+                        ? "/managers/properties"
+                        : "/tenants/favorites",
+                    { scroll: false }
+                )
+            }
+        }
+    })
+
+    if (!authUser?.userRole) return null
+    
+    return (
+        <SidebarProvider>
+            <div className='min-h-screen w-full bg-primary-100'>
+                <Navbar />
+                <div style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
+                    <main className='flex'>
+                        <Sidebar userType={authUser?.userRole.toLowerCase()} />
+                        <div className="flex-grouw transition-all duration-300">
+                            {children}
+                        </div>
+                    </main>
+                </div>
             </div>
-        </div>
-    </SidebarProvider>
-  )
+        </SidebarProvider>
+    )
 }
 
 export default DashboardLayout
--- client/src/components/AppSidebar.tsx
+++ client/src/components/AppSidebar.tsx
@@ -1,10 +1,11 @@
 import { usePathname } from 'next/navigation'
 import React from 'react'
-import { Sidebar, SidebarHeader, SidebarMenu, SidebarMenuItem, useSidebar } from './ui/sidebar'
+import { Sidebar, SidebarContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from './ui/sidebar'
 import { Building, FileText, Heart, Menu, Settings, X } from 'lucide-react'
 import Home from '@/app/page'
 import { NAVBAR_HEIGHT } from '@/lib/constants'
 import { cn } from '@/lib/utils'
+import Link from 'next/link'
 
 const AppSidebar = ({ userType }: AppSidebarProps) => {
   const pathname = usePathname()
@@ -73,6 +74,48 @@ const AppSidebar = ({ userType }: AppSidebarProps) => {
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
+      <SidebarContent>
+        <SidebarMenu>
+          {navLinks.map((link) => {
+            const isActive = pathname === link.href
+
+            return (
+              <SidebarMenuItem key={link.href}>
+                <SidebarMenuButton
+                  asChild
+                  className={cn(
+                    "flex items-center px-7 py-7",
+                    isActive
+                      ? "bg-gray-100"
+                      : "text-gray-600 hover:bg-gray-100",
+                    open ? "text-blue-600" : "ml-[5px]"
+                  )}>
+
+                  <Link href={link.href} className='w-full' scroll={false}>
+                    <div className="flex items-center gap-3">
+                      <link.icon
+                        className={`h-5 w-5 ${
+                          isActive ? "text-blue-600": "text-gray-600"
+                        }`}
+                      />
+                      <span
+                        className={`font-medium ${
+                          isActive ? "text-blue-600": "text-gray-600"
+                        }`}
+                      >
+                        {link.label}
+                      </span>
+                    </div>
+                  </Link>
+
+                </SidebarMenuButton>
+
+              </SidebarMenuItem>
+            )
+
+          })}
+        </SidebarMenu>
+      </SidebarContent>
     </Sidebar>
   )
 }
