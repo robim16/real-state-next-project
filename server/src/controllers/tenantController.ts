b772917e4b44d2b61dff5aeebd16752e4a29b809
robim16 <carteagajimenez@gmail.com>
2025-07-31 15:43:22 -0500

    creacion del método get current residences

--- server/src/controllers/tenantController.ts
+++ server/src/controllers/tenantController.ts
@@ -64,3 +64,52 @@ export const updateTenant = async (req: Request, res: Response): Promise<void> =
         res.status(500).json({ message: `Error updating tenant: ${error.message}` })
     }
 }
+
+
+export const getCurrentResidences = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    try {
+        const { cognitoId } = req.params;
+       
+
+        const properties = await prisma.property.findMany({
+            where: { tenants: { some: { cognitoId }} },
+            include: {
+                location: true
+            }
+        })
+
+        const propertiesWithFormattedLocation = await Promise.all(
+            properties.map(async (property) => {
+                const coordinates: { coordinates: string }[] =
+                    await prisma.$queryRaw`SELECT ST_asText(coordinates) as coordinates from "Location" where id = ${property.location.id}`;
+
+                const geoJSON: any = wktToGeoJSON(coordinates[0]?.coordinates || "");
+                const longitude = geoJSON.coordinates[0];
+                const latitude = geoJSON.coordinates[1];
+
+                return {
+                    ...property,
+                    location: {
+                        ...property.location,
+                        coordinates: {
+                            longitude,
+                            latitude,
+                        },
+                    },
+                };
+            })
+        )
+
+       
+
+        res.json(propertiesWithFormattedLocation);
+        
+    } catch (err: any) {
+        res
+        .status(500)
+        .json({ message: `Error retrieving manager properties: ${err.message}` });
+    }
+}
