58c6bdaea9f2d8755eeb169c1c0b80cc6bb4431f
robim16 <carteagajimenez@gmail.com>
2025-10-11 15:40:50 -0500

    creacion del comp. new property

--- client/src/app/(nondashboard)/search/[id]/PropertyLocation.tsx
+++ client/src/app/(nondashboard)/search/[id]/PropertyLocation.tsx
@@ -1,9 +1,10 @@
-import { AmenityIcons, HighlightIcons } from "@/lib/constants";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import { useGetPropertyQuery } from "@/state/api";
-import { HelpCircle } from "lucide-react";
-import React from "react";
-import { formatEnumString } from "@/lib/utils";
+import { Compass, MapPin } from "lucide-react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import React, { useEffect, useRef } from "react";
+
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
 
@@ -13,6 +14,7 @@ const PropertyLocation = ({ propertyId }: PropertyDetailsProps) => {
         isError,
         isLoading,
     } = useGetPropertyQuery(propertyId);
+    const mapContainerRef = useRef(null)
 
     if (isLoading) return <>Loading...</>;
     if (isError || !property) {
@@ -20,34 +22,64 @@ const PropertyLocation = ({ propertyId }: PropertyDetailsProps) => {
     }
 
     useEffect(() => {
-        if (isLoading || isError || !properties) return;
+        if (isLoading || isError || !property) return;
 
         const map = new mapboxgl.Map({
             container: mapContainerRef.current!,
             style: "mapbox://styles/majesticglue/cm6u301pq008b01sl7yk1cnvb",
-            center: filters.coordinates || [-74.5, 40],
-            zoom: 9,
+            center: [
+                property.location.coordinates.longitude,
+                property.location.coordinates.latitude
+            ],
+            zoom: 14,
         });
 
-        properties.forEach((property) => {
-            const marker = createPropertyMarker(property, map);
-            const markerElement = marker.getElement();
-            const path = markerElement.querySelector("path[fill='#3FB1CE']");
-            if (path) path.setAttribute("fill", "#000000");
-        });
+        const marker = new mapboxgl.Marker()
+            .setLngLat([
+                property.location.coordinates.longitude,
+                property.location.coordinates.latitude
+            ])
+            .addTo(map);
 
-        const resizeMap = () => {
-            if (map) setTimeout(() => map.resize(), 700);
-        };
-        resizeMap();
+        const markerElement = marker.getElement()
+        const path = markerElement.querySelector("path[fill='#3FB1CE']")
+
+        if (path) path.setAttribute("fill", "#000000")
+
 
         return () => map.remove();
-    }, [isLoading, isError, properties, filters.coordinates]);
-    
-    return (
-        <div className="mb-6">
+    }, [property, isError, isLoading]);
 
+    return (
+        <div className="py-16">
+      <h3 className="text-xl font-semibold text-primary-800 dark:text-primary-100">
+        Map and Location
+      </h3>
+      <div className="flex justify-between items-center text-sm text-primary-500 mt-2">
+        <div className="flex items-center text-gray-500">
+          <MapPin className="w-4 h-4 mr-1 text-gray-700" />
+          Property Address:
+          <span className="ml-2 font-semibold text-gray-700">
+            {property.location?.address || "Address not available"}
+          </span>
         </div>
+        <a
+          href={`https://maps.google.com/?q=${encodeURIComponent(
+            property.location?.address || ""
+          )}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex justify-between items-center hover:underline gap-2 text-primary-600"
+        >
+          <Compass className="w-5 h-5" />
+          Get Directions
+        </a>
+      </div>
+      <div
+        className="relative mt-4 h-[300px] rounded-lg overflow-hidden"
+        ref={mapContainerRef}
+      />
+    </div>
     );
 };
 
--- client/src/app/(nondashboard)/search/[id]/page.tsx
+++ client/src/app/(nondashboard)/search/[id]/page.tsx
@@ -2,14 +2,17 @@
 
 import { useGetAuthUserQuery } from '@/state/api'
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 import ImagePreviews from './ImagePreviews'
 import PropertyOverview from './PropertyOverview'
 import PropertyDetails from './PropertyDetails'
+import PropertyLocation from './PropertyLocation'
+import ContactWidget from './ContactWidget'
 
 const SingleListing = () => {
   const { id } = useParams()
   const propertyId = Number(id)
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const { data: authUser } = useGetAuthUserQuery()
   return (
     <div>
@@ -20,6 +23,9 @@ const SingleListing = () => {
           <PropertyOverview propertyId={propertyId} />
           <PropertyDetails propertyId={propertyId} />
           <PropertyLocation propertyId={propertyId} />
+        </div>
+        <div className="order-1 md:order-2">
+          <ContactWidget onOpenModal={() => setIsModalOpen(true)} />
         </div>
       </div>
     </div>
